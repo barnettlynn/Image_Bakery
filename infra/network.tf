122fdd4fd5cb3891cb94683be8b394e3a532fe48
Lynn Barnett <barnettlynn@gmail.com>
2020-09-09 16:23:15 -0500

    s3 backend

--- infra/network.tf
+++ infra/network.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tfstate-lynnbarnett"
+    key    = "prod"
+    region = "us-east-1"
+  }
+}
 # TODO: ACL
 
 
